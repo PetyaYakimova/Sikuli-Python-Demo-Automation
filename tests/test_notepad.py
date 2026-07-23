c1e4acc22f2a74d5ffddf1e372a20d719c018c42
Petya Yakimova <petya_yakimova@abv.bg>
2026-07-23 08:58:08 +0300

    Update notepad test

--- tests/test_notepad.py
+++ tests/test_notepad.py
@@ -1,9 +1,11 @@
 from pages.notepad_page import NotepadPage
+from utils.logger import logger
 
 
 def test_open_file_menu():
     page = NotepadPage()
 
+    logger.info("Opening File menu")
     input("Open Notepad and press Enter...")
 
     page.open_file_menu()
