cc5cedd2a496a77340f554775817e67bfc06607a
Petya Yakimova <petya_yakimova@abv.bg>
2026-07-26 10:23:37 +0300

    Use image util in notepad page

--- pages/notepad_page.py
+++ pages/notepad_page.py
@@ -1,10 +1,11 @@
 from pages.base_page import BasePage
+from utils.image_utils import image
 
 
 class NotepadPage(BasePage):
 
     def open_file_menu(self):
-        self.screen.click("images/file_menu.png")
+        self.screen.click(image("images/file_menu.png"))
 
     def click_save_as(self):
         self.screen.click("images/save_as.png")
