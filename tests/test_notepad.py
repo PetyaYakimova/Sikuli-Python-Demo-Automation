db486123b0fcc5cc50981713df3b6230995adae1
Petya Yakimova <petya_yakimova@abv.bg>
2026-07-29 09:04:24 +0300

    Add conftest

--- tests/test_notepad.py
+++ tests/test_notepad.py
@@ -9,3 +9,21 @@ def test_open_file_menu():
     input("Open Notepad and press Enter...")
 
     page.open_file_menu()
+
+
+#import subprocess
+#import time
+
+#from pages.notepad_page import NotepadPage
+#from utils.logger import logger
+
+
+#def test_open_file_menu():
+    #logger.info("Starting Notepad")
+
+    #subprocess.Popen("notepad.exe")
+
+    #time.sleep(2)
+
+    #page = NotepadPage()
+    #page.open_file_menu()
