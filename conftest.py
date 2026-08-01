728d3fd752ca63e5e6076c8feea60d35365807ba
Petya Yakimova <77053415+PetyaYakimova@users.noreply.github.com>
2026-08-01 09:24:56 +0300

    Update conftest

--- conftest.py
+++ conftest.py
@@ -1,7 +1,22 @@
 import os
 import sys
+import subprocess
+import time
+
+import pytest
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
 if ROOT not in sys.path:
     sys.path.insert(0, ROOT)
+
+@pytest.fixture
+def notepad():
+    process = subprocess.Popen("notepad.exe")
+
+    time.sleep(2)
+
+    yield process
+
+    process.terminate()
+    process.wait(timeout=5)
