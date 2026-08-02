6018746696a12b15090e059bd46db181f5c4d4fe
Petya Yakimova <77053415+PetyaYakimova@users.noreply.github.com>
2026-08-02 10:05:16 +0300

    update notepad test to use fixture

--- tests/test_notepad.py
+++ tests/test_notepad.py
@@ -5,12 +5,7 @@ from pages.notepad_page import NotepadPage
 from utils.logger import logger
 
 
-def test_open_file_menu():
-    logger.info("Starting Notepad")
-
-    subprocess.Popen("notepad.exe")
-
-    time.sleep(2)
-
+def test_open_file_menu(notepad):
     page = NotepadPage()
+
     page.open_file_menu()
